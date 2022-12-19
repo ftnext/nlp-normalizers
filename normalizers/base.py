c5700abbdf0fbb6472bb5da6aa57a96c6154a991
ftnext <takuyafjp+develop@gmail.com>
2022-12-19 23:51:11 +0900

    [feat] Normalizers can normalize text

--- normalizers/base.py
+++ normalizers/base.py
@@ -2,4 +2,5 @@ from typing import Protocol
 
 
 class Normalizer(Protocol):
-    ...
+    def normalize(self, text: str) -> str:
+        ...
--- tests/test_base.py
+++ tests/test_base.py
@@ -6,3 +6,9 @@ from normalizers.base import Normalizer
 class TestNormalizer:
     def test_is_protocol(self):
         assert issubclass(Normalizer, Protocol)
+
+    def test_has_normalize_method(self):
+        class SpamNormalizer(Normalizer):
+            ...
+
+        assert hasattr(SpamNormalizer(), "normalize")
