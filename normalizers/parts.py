0ef52f9d7d6217eee0e35e9bbd31bd5181001b9c
ftnext <takuyafjp+develop@gmail.com>
2022-12-20 23:52:00 +0900

    [feat] Can strip white spaces

--- normalizers/parts.py
+++ normalizers/parts.py
@@ -2,4 +2,5 @@ from normalizers.base import Normalizer
 
 
 class Strip(Normalizer):
-    ...
+    def normalize(self, text: str) -> str:
+        return text.strip()
--- tests/test_parts.py
+++ tests/test_parts.py
@@ -5,3 +5,8 @@ from normalizers.parts import Strip
 class TestStrip:
     def test_implements_normalizer_protocol(self):
         assert isinstance(Strip(), Normalizer)
+
+    def test_can_strip(self):
+        sut = Strip()
+
+        assert sut.normalize("      テキストの前後      ") == "テキストの前後"
