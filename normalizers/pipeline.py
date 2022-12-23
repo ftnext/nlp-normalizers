0aac97a155943015869c12c24423500ad9bfbc7f
ftnext <takuyafjp+develop@gmail.com>
2022-12-23 15:45:53 +0900

    [feat] Can collect normalizers as Pipeline, then apply them

--- normalizers/pipeline.py
+++ normalizers/pipeline.py
@@ -6,3 +6,8 @@ from normalizers.base import Normalizer
 class Pipeline(Normalizer):
     def __init__(self, normalizers: list[Normalizer]) -> None:
         self.normalizers = normalizers
+
+    def normalize(self, text: str) -> str:
+        for normalizer in self.normalizers:
+            text = normalizer.normalize(text)
+        return text
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -8,3 +8,15 @@ class TestPipeline:
     def test_implements_normalizer_protocol(self):
         normalizers = [MagicMock(spec=Normalizer)]
         assert isinstance(Pipeline(normalizers), Normalizer)
+
+    def test_normalize_by_each_normalizer(self):
+        normalizer1 = MagicMock(spec=Normalizer)
+        normalizer2 = MagicMock(spec=Normalizer)
+        sut = Pipeline([normalizer1, normalizer2])
+        text = MagicMock(spec=str)
+
+        assert sut.normalize(text) == normalizer2.normalize.return_value
+        normalizer1.normalize.assert_called_once_with(text)
+        normalizer2.normalize.assert_called_once_with(
+            normalizer1.normalize.return_value
+        )
